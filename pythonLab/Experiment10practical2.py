be79828d6620410309da5fd73d2e7e8734dd9760
Sanya Srivastava <129599279+Sanya0732@users.noreply.github.com>
2024-01-30 18:18:36 +0530

    Add files via upload

---
+++ pythonLab/Experiment10practical2.py
@@ -0,0 +1,10 @@
+print("enter the first side of your triangle ")
+firstSide=int(input())
+print("enter the second side of your triangle ")
+secondSide=int(input())
+print("enter the third side of your triangle ")
+thirdSide=int(input())
+if(firstSide==secondSide==thirdSide):
+    print("it is an equilateral triangle")
+else:
+    print("it is not an equilateral triangle")
